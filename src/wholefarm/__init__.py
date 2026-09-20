b4dff05162f4c0c5cd23b5a9635fa3dda68d5220
danmulat <61710372+danmulat@users.noreply.github.com>
2026-09-20 11:56:59 +0300

    Export integrated farm model interfaces

--- src/wholefarm/__init__.py
+++ src/wholefarm/__init__.py
@@ -2,15 +2,39 @@
 
 from .accounting import CarbonStockChange, FarmGHGInventory, FarmScenarioResult, ScenarioComparison
 from .config import ModelConfig
+from .farm_pipeline import (
+    FarmScenarioInputs,
+    build_farm_scenario,
+    compare_farm_scenarios,
+    soc_stock_change_co2e_t,
+)
+from .livestock_farm import (
+    DietEmissionFactors,
+    LivestockCohortSpec,
+    LivestockFarmResult,
+    run_livestock_farm,
+)
+from .nutrient_flow import ManureRecoveryConfig, ManureNutrientFlow, route_manure_to_fields
 from .soc_stock import SoilLayer, aggregate_soc_layers_t_c_ha, soc_stock_t_c_ha
 
 __all__ = [
     "CarbonStockChange",
+    "DietEmissionFactors",
     "FarmGHGInventory",
+    "FarmScenarioInputs",
     "FarmScenarioResult",
+    "LivestockCohortSpec",
+    "LivestockFarmResult",
+    "ManureNutrientFlow",
+    "ManureRecoveryConfig",
     "ModelConfig",
     "ScenarioComparison",
     "SoilLayer",
     "aggregate_soc_layers_t_c_ha",
+    "build_farm_scenario",
+    "compare_farm_scenarios",
+    "route_manure_to_fields",
+    "run_livestock_farm",
+    "soc_stock_change_co2e_t",
     "soc_stock_t_c_ha",
 ]
