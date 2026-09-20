bb827a09d8521b2c04adbc6928c28a107ae7cb63
danmulat <61710372+danmulat@users.noreply.github.com>
2026-09-20 12:47:42 +0300

    Export equivalent soil mass utilities

--- src/wholefarm/__init__.py
+++ src/wholefarm/__init__.py
@@ -15,7 +15,15 @@ from .livestock_farm import (
     run_livestock_farm,
 )
 from .nutrient_flow import ManureNutrientFlow, ManureRecoveryConfig, route_manure_to_fields
-from .soc_stock import SoilLayer, aggregate_soc_layers_t_c_ha, soc_stock_t_c_ha
+from .soc_stock import (
+    SoilLayer,
+    aggregate_soc_layers_t_c_ha,
+    equivalent_soil_mass_change_t_c_ha,
+    equivalent_soil_mass_soc_t_c_ha,
+    fine_soil_mass_t_ha,
+    reference_soil_mass_t_ha,
+    soc_stock_t_c_ha,
+)
 
 __all__ = [
     "CarbonStockChange",
@@ -31,6 +39,10 @@ __all__ = [
     "ScenarioComparison",
     "SoilLayer",
     "aggregate_soc_layers_t_c_ha",
+    "equivalent_soil_mass_change_t_c_ha",
+    "equivalent_soil_mass_soc_t_c_ha",
+    "fine_soil_mass_t_ha",
+    "reference_soil_mass_t_ha",
     "build_farm_scenario",
     "compare_farm_scenarios",
     "route_manure_to_fields",
